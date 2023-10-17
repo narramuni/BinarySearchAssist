98b93a8cfa4019d58644f906cfd8bb00f22f5ee5
narramuni <nmspavan@gmail.com>
2023-10-17 16:51:19 +0530

    Add project files.

---
+++ BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchAssist
+{
+    public class BinarySearch
+    {
+       
+
+        public static int BinarySearchValues(int[] sortedArray, int target)
+        {
+            int left = 0;
+            int right = sortedArray.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (sortedArray[mid] == target)
+                {
+                    return mid; // Element found, return the index
+                }
+
+                if (sortedArray[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1; // Element not found
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        static void runApp()
+
+        {
+            
+
+            int[] sortedArray = { 5, 7, 12, 15, 28, 34, 47, 50, 62, 89 };
+            Console.WriteLine("Sorted Array values: " + string.Join(", ", sortedArray));
+
+            Console.Write("Enter a value to search for: ");
+            int target = int.Parse(Console.ReadLine());
+
+            int index = BinarySearch.BinarySearchValues(sortedArray, target);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"{target} found at index {index}.");
+            }
+            else
+            {
+                Console.WriteLine($"{target} not found in the array.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
